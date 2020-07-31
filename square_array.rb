dd7af425d05cb47fc7f10665544984fb3bce0b04
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-07-31 03:40:55 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,10 @@
 # your code here
 build a method, square_array
 squares each element in an array of numbers
+returns a new array of these squared numbers
+
+
+
 
 def square_array(array)
   new_numbers = array*array
